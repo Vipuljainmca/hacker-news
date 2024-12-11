9960dd54c53461bf510aa34212e4bb7eed79f676
Vipuljainmca <144492088+Vipuljainmca@users.noreply.github.com>
2024-12-11 15:32:09 +0530

    changes

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,16 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import process from 'process';
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAm7TEMDrbEMas9DiGVeRvvyxcPiUDR22M",
-//     authDomain: "surverguy-cecb0.firebaseapp.com",
-//     projectId: "surverguy-cecb0",
-//     storageBucket: "surverguy-cecb0.firebasestorage.app",
-//     messagingSenderId: "911371357449",
-//     appId: "1:911371357449:web:147c14de0bfe4c9cb557d8",
-//     measurementId: "G-BBJGLGHT6V"
-// };
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
