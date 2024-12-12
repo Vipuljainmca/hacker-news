f320b1cd309eccaec47d99861503d63f7a52cbf7
Vipuljainmca <144492088+Vipuljainmca@users.noreply.github.com>
2024-12-12 13:26:19 +0530

    add changes navigation

--- src/components/RouteHome.jsx
+++ src/components/RouteHome.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './Login';
-import SearchBar from './SearchBar';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Login";
+import SearchBar from "./SearchBar";
 
 const RouteHome = () => {
-    const isAuthenticated = useSelector((state) => state?.auth?.isAuthenticated);
-    
+    const isAuthenticated = useSelector(state => state?.auth?.isAuthenticated);
 
     return (
-       
         <Routes>
-        {isAuthenticated && <Route path="/login" element={<Navigate to="/" />} />}
+            {isAuthenticated && (
+                <Route path="/login" element={<Navigate to="/" />} />
+            )}
 
-        {!isAuthenticated && <Route path="/" element={<Navigate to="/login" />} />}
+            {!isAuthenticated && (
+                <Route path="/" element={<Navigate to="/login" />} />
+            )}
 
-        <Route path="/" element={<SearchBar />} />
-                    <Route path="/search" element={<SearchBar />} />
-                    <Route path="/search_by_date" element={<SearchBar />} />
-                    <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/" element={<SearchBar />} />
+            <Route path="/search" element={<SearchBar />} />
+            <Route path="/search_by_date" element={<SearchBar />} />
+            <Route path="*" element={<Navigate to="/" />} />
 
-        {!isAuthenticated && <Route path="*" element={<Navigate to="/login" />} />}
-        {isAuthenticated && <Route path="*" element={<Navigate to="/" />} />}
-    </Routes>
+            {!isAuthenticated && (
+                <Route path="*" element={<Navigate to="/login" />} />
+            )}
+            {isAuthenticated && (
+                <Route path="*" element={<Navigate to="/" />} />
+            )}
+        </Routes>
     );
 };
 
